1d4c96f4f0ac79bc1cbcb8b339b9c98cdb3e4781
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-02-08 17:41:09 +0200

    Oval

---
+++ src/Oval.java
@@ -0,0 +1,6 @@
+public class Oval implements Shape {
+    @Override
+    public void name() {
+        System.out.println("Oval");
+    }
+}
--- src/Shape.java
+++ src/Shape.java
@@ -1,25 +1,3 @@
-interface Shape{
-    void takesShape();
-
-}
-interface Circle extends Shape{
-
-}
-interface Square extends Shape{
-
-}
-interface Triangle extends Shape{
-
-}
-interface Diamond extends Shape{
-
-}
-interface Oval extends Shape{
-
-}
-interface Pap extends Shape{
-
-}
-interface Adfs extends Shape{
-
+interface Shape {
+    void name();
 }
