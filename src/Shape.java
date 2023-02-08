3e07b7d324baf139f92a02d05bcb2e503d0fc598
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-02-08 17:14:12 +0200

    Initial commit

---
+++ src/Shape.java
@@ -0,0 +1,22 @@
+interface Shape{
+    void takesShape();
+
+}
+interface Circle extends Shape{
+
+}
+interface Square extends Shape{
+
+}
+interface Triangle extends Shape{
+
+}
+interface Diamond extends Shape{
+
+}
+interface Oval extends Shape{
+
+}
+interface Pap extends Shape{
+    
+}
